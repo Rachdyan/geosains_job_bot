bd3e9fa9ef8a480ecf3e1a9f0c978c20c42c0eec
InnovativeOptics <rachdyannaufal@gmail.com>
2025-05-18 01:01:23 +0700

    update indeed

--- scrape_indeed.py
+++ scrape_indeed.py
@@ -101,7 +101,7 @@ urls = ["https://id.indeed.com/jobs?q=geology&sort=date",
         ]
 
 if __name__ == "__main__":
-    with SB(uc=True, headless=True, xvfb=True,
+    with SB(uc=True, headless=False, xvfb=True,
             proxy=proxy_string,
             maximize=True,
             ) as sb:
@@ -144,7 +144,7 @@ if __name__ == "__main__":
         # sb.open("https://id.indeed.com")
         sb.driver.uc_open_with_reconnect("https://id.indeed.com",
                                          reconnect_time=10)
-        sb.driver.uc_gui_handle_cf()
+        sb.uc_gui_click_cf()
         sb.sleep(3)
         all_jobs_df = pd.concat([
             get_job_from_indeed_url(
